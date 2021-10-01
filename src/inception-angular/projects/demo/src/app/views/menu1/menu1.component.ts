27ac205ad7487e1878a4b13b125fd6452399b95e
Marcus Portmann <marcus@mmp.guru>
2021-10-01 19:33:07 +0200

    Fixed SessionInterceptor issue

--- src/inception-angular/projects/demo/src/app/views/menu1/menu1.component.ts
+++ src/inception-angular/projects/demo/src/app/views/menu1/menu1.component.ts
@@ -14,15 +14,7 @@
  * limitations under the License.
  */
 
-import {
-  AccessDeniedError, CommunicationError, INCEPTION_CONFIG, InceptionConfig, ServiceUnavailableError
-} from "@absaoss/ngx-inception/core";
-import {Country, ReferenceService} from "@absaoss/ngx-inception/reference";
-import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
-import {Component, Inject, LOCALE_ID} from '@angular/core';
-import {Observable, throwError} from "rxjs";
-import {catchError, map} from "rxjs/operators";
-
+import {Component} from '@angular/core';
 
 /**
  * The Menu1Component class implements the menu 1 component.
@@ -40,40 +32,6 @@ import {catchError, map} from "rxjs/operators";
 })
 export class Menu1Component {
 
-  constructor(@Inject(INCEPTION_CONFIG) private config: InceptionConfig,
-              @Inject(LOCALE_ID) private localeId: string, private httpClient: HttpClient,
-              private referenceService: ReferenceService) {
-
-    this.doIt().subscribe((countries) => {
-      console.log('countries = ', countries);
-    });
-
-
-    // referenceService.getCountries().pipe(first()).subscribe((countries: Country[]) => {
-    //   console.log('countries = ', countries);
-    // });
-
-  }
-
-  doIt(): Observable<Country[]> {
-    {
-      let params = new HttpParams();
-
-      params = params.append('localeId', this.localeId);
-
-      return this.httpClient.get<Country[]>(this.config.referenceApiUrlPrefix + '/countries',
-        {params, reportProgress: true})
-      .pipe(map((countries: Country[]) => {
-        return countries;
-      }), catchError((httpErrorResponse: HttpErrorResponse) => {
-        if (AccessDeniedError.isAccessDeniedError(httpErrorResponse)) {
-          return throwError(new AccessDeniedError(httpErrorResponse));
-        } else if (CommunicationError.isCommunicationError(httpErrorResponse)) {
-          return throwError(new CommunicationError(httpErrorResponse));
-        }
-
-        return throwError(new ServiceUnavailableError('Failed to retrieve the countries.', httpErrorResponse));
-      }));
-    }
+  constructor() {
   }
 }
